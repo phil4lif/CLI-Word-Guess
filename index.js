64e95a418ee3eaf5849c636a7c215702fbaff1c0
phil cowan <philipjcowan1@gmail.com>
2020-02-19 13:32:44 -0700

    gets the majority of the game functioning, need some functionality for the game ending

--- index.js
+++ index.js
@@ -3,36 +3,60 @@ var inquirer = require("inquirer");
 var Letter = require("./letter");
 //an array of words for the wordGen function to select from
 var randomWord = ["javascript", "materialize", "node", "bootstrap", "firebase"]
+var alphabetArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wrongGuess = [];
+var rightGuess = [];
+var guessesLeft = 12;
+var wordComplete = [];
+var chosenWord = randomWord[Math.floor(Math.random() * randomWord.length)].toLowerCase();
 //the function to select a randomWord index
+
+var newWord;
 function wordGen() {
-    var word = randomWord[Math.floor(Math.random() * randomWord.length)].toLowerCase();
-    // console.log(word);
-    var newWord = new Word(word);
-    console.log(newWord)
+
+    return new Promise(function (resolve, reject) {
+        // console.log(word);
+        newWord = new Word(chosenWord, wordComplete, wrongGuess, guessesLeft);
+        //   console.log(chosenWord)
+        newWord.display(chosenWord)
+        resolve()
+    })
 }
-//invoke the wordGen function
-wordGen()
 
 
 
-var count = 0;
+
 var askQuestion = function () {
-    if (count < letters.length) {
+
+    wordGen().then(function () {
+
         //prompts the player for a guess
         inquirer.prompt([
             {
+                type: "input",
                 name: "guess",
                 message: "Guess a Letter"
             }
         ])
             .then(function (answer) {
-                var newGuess = answer;
-
-                Letter.checkGuess(newGuess);
-                count++;
+                var newGuess = answer.guess;
+                // if(newGuess !=)
+                console.log(newGuess)
+                var newGame = newWord.userGuess(newGuess)
+                guessesLeft = newGame;
+                if (newGame === 0) {
+                    wrongGuess = [];
+                    guessesLeft = 12;
+                    wordComplete = [];
+                    chosenWord = randomWord[Math.floor(Math.random() * randomWord.length)].toLowerCase();
+                }
+                // count++;
                 askQuestion();
 
             })
-    }
+    })
+
+
 }
-askQuestion();
+
+askQuestion()
--- word.js
+++ word.js
@@ -1,22 +1,64 @@
 var Letter = require("./letter")
-function Word(word){
-    this.wordArr = [],
 
-    this.makeLetters = function(){
-    var letters = this.word.split("");
-    // console.log(letters)
-    for (var i = 0; i<letters.length; i++){
-    var newLetter = new Letter(letters[i]);
-    }
-    console.log(letters)
+function Word(word, arrayboard, wrongGuess, guessesLeft) {
 
-    }
-    this.userGuess = function(input){
-        for(var i = 0; i<this.wordArr.length; i++){
-            this.wordArr[i].guess(input)
+   this.wrongGuess = wrongGuess;
+   console.log(guessesLeft)
+    this.guessesLeft=guessesLeft;
+    this.letterArr = arrayboard;
+
+
+    this.word = word;
+    console.log(this.word)
+    // // console.log(word.length)
+    // for (var i = 0; i < word.length; i++) {
+    //     var newLetter = new Letter(word[i]);
+    //     // console.log(newLetter)
+    // }
+    this.display = function (chosenWord) {
+        var display = "";
+        if (this.letterArr.length === 0) {
+            for (var i = 0; i < chosenWord.length; i++) {
+                this.letterArr.push("_")
+            }
         }
+
+        console.log(this.letterArr.join(" "));
+    };
+
+
+
+    this.userGuess = function (guess) {
+
+        var chosenWordArr = this.word.split("");
+        // console.log(chosenWordArr)
+        // console.log(guess)
+        for (var i = 0; i < chosenWordArr.length; i++) {
+            if (chosenWordArr[i] === guess) {
+                this.letterArr[i] = guess
+            }
+        }
+
+        if(this.letterArr.indexOf("_")===-1){
+            console.log("YOU WON")
+        }
+       
+        if(chosenWordArr.indexOf(guess)===-1){
+            if(this.wrongGuess.indexOf(guess)===-1){
+                this.guessesLeft--;
+                console.log("remaining Guesses: " + this.guessesLeft)
+                wrongGuess.push(guess);
+                console.log(wrongGuess.join(","))
+            }
+        }
+
+        if(this.guessesLeft === 0){
+            console.log("game over")
+            return this.guessesLeft;
+        }
+        // console.log(this.letterArr);
+        return  this.guessesLeft;
     }
 }
 // newWord.makeLetters();
 module.exports = Word
-module.exports = Letter
